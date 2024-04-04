70e3cce3390d0a81bb00777d8646e159dbaf4507
Muhammad Ayan <159698909+MuhammadAyan67@users.noreply.github.com>
2024-04-05 04:02:00 +0500

    Add files via upload

---
+++ game.ts
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer"
+
+const randomNuber = Math.floor(Math.random() * 5 + 1);
+const answer = await inquirer.prompt([
+       {
+    name : "guessNumber",
+    type : "number",
+    message : "Enter your guess number (num limit from 1 to 5) :  "
+       },
+])
+if (answer.guessNumber === randomNuber){
+    console.log("Congratulation ! You Guess a Correct Number")
+}else{
+    console.log("You Guess a Wrong Nmuber ")
+} 
+
